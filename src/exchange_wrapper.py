b12bf9c48db1a0785a266319de31c1dbf0a6c420
Sandy Kusuma <falcon2step@gmail.com>
2026-06-08 21:11:34 +0700

    fix: revert to enable_demo_trading() - server /etc/hosts fixes demo-fapi connectivity

--- src/exchange_wrapper.py
+++ src/exchange_wrapper.py
@@ -37,17 +37,7 @@ class ExchangeWrapper:
             }
         self.exchange = exchange_class(opts)
         if is_futures and self.testnet:
-            # demo-fapi.binance.com blocked by server firewall; use working testnet URL
-            self.exchange.urls['api'] = {
-                'public': 'https://testnet.binancefuture.com/fapi/v1',
-                'private': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPublic': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPrivate': 'https://testnet.binancefuture.com/fapi/v1',
-                'fapiPublicV2': 'https://testnet.binancefuture.com/fapi/v2',
-                'fapiPrivateV2': 'https://testnet.binancefuture.com/fapi/v2',
-                'fapiPublicV3': 'https://testnet.binancefuture.com/fapi/v3',
-                'fapiPrivateV3': 'https://testnet.binancefuture.com/fapi/v3',
-            }
+            self.exchange.enable_demo_trading(True)
         await self.exchange.load_markets()
         await self.exchange.load_time_difference()
         self.exchange.options['adjustForTimeDifference'] = True
